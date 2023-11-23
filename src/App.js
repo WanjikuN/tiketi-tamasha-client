4b8a0dc9a865947ece81c924c84d07a6215ae13c
Gideon <gideonnemugere@gmail.com>
2023-11-23 14:57:06 +0300

    Components folder added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import './components/App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
