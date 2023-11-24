793385f94b2a484612b4c5ebcc604461135c01b1
Obadia <captainkimmaasai@gmail.com>
2023-11-24 10:19:08 +0300

    signup part

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './components/App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import SignUp from './components/SignUp';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Footer />
+      <SignUp/>
     </div>
   );
 };
