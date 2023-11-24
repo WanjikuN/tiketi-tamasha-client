1261bd5b74844e7713c8b129526bd989f1525236
WanjikuN <cikunjoroge4@gmail.com>
2023-11-24 10:02:05 +0300

    install add to calendar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './components/App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Footer />
+      <LandingPage />
     </div>
   );
 };
