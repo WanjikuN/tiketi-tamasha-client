7ccdcdfbb62b3c0653326f34795f92af5121ef33
Gideon <gideonnemugere@gmail.com>
2023-11-24 12:12:41 +0300

    updated Navbar, AboutUs and Readme Documentation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './components/App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import AboutUs from './components/AboutUs';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <AboutUs />
       <Footer />
     </div>
   );
---
+++ src/components/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-page">
+      <div className="container">
+        <h1>About Us</h1>
+        <p>Revolutionize Your Ticketing Experience with Tiketi Tamasha.</p>
+        <section className="who-we-are">
+          <h2>Thrive With Us!...</h2>
+          <p>Unlock the potential of your online trade effortlessly with Tiketi Tamasha. Our platform, designed with simplicity in mind, empowers you to swiftly launch into the world of online trading. In just minutes, you can go live using our user-friendly backend dashboard, providing you with unparalleled control over your virtual storefront.</p>
+          <p>With TIketi Tamasha, managing and controlling your online sales becomes a breeze. Our easy-to-use backend dashboard puts you in the driver's seat, allowing you to navigate and optimize your sales journey effortlessly. Let Tiketi Tamasha be the driving force behind your online success. Start your journey today!"</p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
