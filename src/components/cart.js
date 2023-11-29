67581c7297b5f92c5e3461a73c8dff1ce1413d91
WanjikuN <cikunjoroge4@gmail.com>
2023-11-29 11:57:45 +0300

    feat: increment and decrement quantity

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ShoppingCart.css';
-import { useState } from 'react';
-import {  NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
 
 export default function ShoppingCart({ cart, onClose, removeFromCart }) {
-  console.log('Cart items:', cart);
+  const [quantities, setQuantities] = useState({});
+
   const calculateAmount = (item) => {
     if (item.ticketType === 'Regular') {
       return parseFloat(item.regular_price);
@@ -14,41 +14,74 @@ export default function ShoppingCart({ cart, onClose, removeFromCart }) {
       return parseFloat(item.early_booking_price);
     }
   };
+
   const calculateTotalPrice = () => {
-    return cart.reduce((total, item) => total + calculateAmount(item), 0);
-};
+    return cart.reduce(
+      (total, item) => total + (calculateAmount(item)* (quantities[item.cartItemId] || 1)) ,
+      0
+    );
+  };
+
+  const incrementQuantity = (cartItemId) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [cartItemId]: (prevQuantities[cartItemId] || 1) + 1,
+    }));
+  };
+
+  const decrementQuantity = (cartItemId) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [cartItemId]: Math.max((prevQuantities[cartItemId] || 1) - 1, 0),
+    }));
+  };
+
   return (
     <div className="shopping-cart">
       <div className="cart-header">
         <h3>Shopping Cart</h3>
-        <button id="close" onClick={onClose}>X</button>
+        <button id="close" onClick={onClose}>
+          X
+        </button>
       </div>
       <div className="cart-items">
-      <h2 id="total">Total: $ {calculateTotalPrice().toFixed(2)}</h2>
-      {
-           cart.length === 0 ? null: <NavLink to="/checkout" style={{color: "black"}}><h2>Proceed to Checkout</h2></NavLink> 
-            }
+        <h2 id="total">Total: $ {calculateTotalPrice().toFixed(2)}</h2>
+        {cart.length !== 0 && (
+          <NavLink to="/checkout" style={{ color: 'black' }}>
+            <h2>Proceed to Checkout</h2>
+          </NavLink>
+        )}
         {cart.length === 0 ? (
           <p>Cart is empty</p>
         ) : (
           cart.map((item) => (
-            <div id="cart_details" key={item.id+item.ticketType}>
+            <div id="cart_details" key={item.id + item.ticketType}>
               <div id="cart_img">
                 <img id="c_img" src={item.images} alt={item.event_name} />
               </div>
               <div id="cart_dets">
                 <h4>{item.event_name}</h4>
                 <p>{item.ticketType}</p>
-                <p>${calculateAmount(item).toFixed(2)}</p>
+                <p>${(calculateAmount(item).toFixed(2))* (quantities[item.cartItemId] || 1)}</p>
               </div>
-              <div id="inc">
+              <div id="quantity-controls">
+                <button onClick={() => decrementQuantity(item.cartItemId)}>-</button>
+                <span>{quantities[item.cartItemId] || 1}</span>
+                <button onClick={() => incrementQuantity(item.cartItemId)}>+</button>
               </div>
               <div id="delete">
-              <button id="close" style={{ backgroundColor: "white", color: "black" }} onClick={() => removeFromCart(item.cartItemId)}>X</button>
+                <button
+                  id="close"
+                  style={{ backgroundColor: 'white', color: 'black' }}
+                  onClick={() => removeFromCart(item.cartItemId)}
+                >
+                  X
+                </button>
               </div>
             </div>
           ))
         )}
+        
       </div>
     </div>
   );
