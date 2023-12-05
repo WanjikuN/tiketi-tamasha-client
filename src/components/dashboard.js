033d07b34133220854e056ab85d1e67b8bbc5deb
kevin <bettkevin965@gmail.com>
2023-12-05 10:50:25 +0300

    Update the admin's dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,34 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/admin/dashboard')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-  
+  fetch('/admin/dashboard')
+    .then(response => response.json())
+    .then(data => {
+      displayData(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
 
-        displayEventData(data.events);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+  function displayData(data) {
+    const eventsList = document.getElementById('events-list');
+    const usersList = document.getElementById('users-list');
+    const paymentsList = document.getElementById('payments-list');
+
+    data.events.forEach(event => {
+      const eventDiv = document.createElement('div');
+      eventDiv.classList.add('item');
+      eventDiv.textContent = `${event.event_name} - ${event.description}`;
+      eventsList.appendChild(eventDiv);
+    });
+
+    data.users.forEach(user => {
+      const userDiv = document.createElement('div');
+      userDiv.classList.add('item');
+      userDiv.textContent = `${user.username} - ${user.email}`;
+      usersList.appendChild(userDiv);
+    });
+
   
-    function displayEventData(events) {
-      const eventList = document.getElementById('event-list');
-  
-      const eventItems = events.map(event => {
-        return `
-          <div class="event-card">
-            <h2>${event.event_name}</h2>
-            <p><strong>Start Time:</strong> ${event.start_time}</p>
-            <p><strong>End Time:</strong> ${event.end_time}</p>
-            <p><strong>Location:</strong> ${event.location}</p>
-            <p><strong>Description:</strong> ${event.description}</p>
-            <p><strong>MVP Price:</strong> ${event.mvp_price}</p>
-            <!-- Add more details as needed -->
-          </div>
-        `;
-      });
-  
-      eventList.innerHTML = eventItems.join('');
-    }
-  });
-  
+    data.payments.forEach(payment => {
+      const paymentDiv = document.createElement('div');
+      paymentDiv.classList.add('item');
+      paymentDiv.textContent = `${payment.payment_type} - ${payment.status}`;
+      paymentsList.appendChild(paymentDiv);
+    });
+  }
+});
