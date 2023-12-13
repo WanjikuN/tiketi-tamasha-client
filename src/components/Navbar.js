7059c31953a3157fc49a35fe00f3c1eb998b7a5d
WanjikuN <cikunjoroge4@gmail.com>
2023-12-13 08:36:07 +0300

    Fix: navbar navigations based on roles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,7 +68,7 @@ const Navbar = ({ cartLength, cart, removeFromCart, isLoggedIn ,setIsLoggedIn,us
                 </li>
               </>
             )}
-            {userData && userData.userData && userData.userData.role_id === 2 && (
+            {userData && userData.userData && userData.userData.role_id === 2 && isLoggedIn &&(
               <>
                
                 <Link className="nav-item nav-link" to='/about-us'>
@@ -83,7 +83,7 @@ const Navbar = ({ cartLength, cart, removeFromCart, isLoggedIn ,setIsLoggedIn,us
                
               </>
             )}
-            {(userData && userData.userData && userData.userData.role_id === 3)  && (
+            {(userData && userData.userData && userData.userData.role_id === 3 && isLoggedIn)  && (
               <>
                      
               <Link to='/' className="nav-item nav-link"> Home</Link>
@@ -110,7 +110,7 @@ const Navbar = ({ cartLength, cart, removeFromCart, isLoggedIn ,setIsLoggedIn,us
                 
               </>
             )}
-            {(!userData || !userData.userData) &&(
+            {(!userData || !userData.userData || !isLoggedIn ) &&(
               <>
                
               <Link to='/' className="nav-item nav-link"> Home</Link>
