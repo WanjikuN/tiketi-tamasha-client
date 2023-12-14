456ddc8cb763ca7d23a9e5f61880b0dd8fcb49fb
WanjikuN <cikunjoroge4@gmail.com>
2023-12-14 19:48:44 +0300

    update backend url

--- src/components/OrdersHistory.js
+++ src/components/OrdersHistory.js
@@ -76,7 +76,7 @@ const OrderHistory = () => {
         setAuthToken(userAuthToken);
 
         // Fetch processed events from the backend
-        const apiUrl = 'http://127.0.0.1:5000/payments';
+        const apiUrl = 'https://tiketi-tamasha-backend.onrender.com/payments';
 
         console.log('Before fetch:', apiUrl);
 
