f93272f9dbcec8ae2d30bc3b95dcb78ce21052f0
WanjikuN <cikunjoroge4@gmail.com>
2023-12-14 18:53:45 +0300

    update backend url

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -19,7 +19,7 @@ const EventDetails = ({carts, removeFromCart}) => {
   const [successMessage, setSuccessMessage] = useState('');
 
   useEffect(() => {
-    fetch(`http://127.0.0.1:5000/events/${eventId}`)
+    fetch(`https://tiketi-tamasha-backend.onrender.com/events/${eventId}`)
       .then((res) => res.json())
       .then((data) => {
         setEventDetails(data);
